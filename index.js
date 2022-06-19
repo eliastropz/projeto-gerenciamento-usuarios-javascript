bb1113a03366571f2fc98c00da8a79f4ccc403be
Elias Siqueira <eliaslsiqueira@gmail.com>
2022-06-19 18:03:06 -0400

    For Each

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+
+
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(fields, index){
+
+    console.log(fields.id, fields.name, fields.value, fields.checked, index);
+});
